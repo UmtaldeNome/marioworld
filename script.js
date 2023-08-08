79e09795fe6c312d63f67d7a620e848f9911b26a
UmtaldeNome <141534156+UmtaldeNome@users.noreply.github.com>
2023-08-08 18:24:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 
 
+let mili = 0
+let seconds = 0
+
+
 const jump = () => {
     mario.classList.add('jump')
     setTimeout(() => {
@@ -9,11 +13,12 @@ const jump = () => {
     },500)
 }
 
+
 const loop = setInterval(() =>{
     const pipePosition = pipe.offsetLeft
     const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '')
-
     
+
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
@@ -26,7 +31,20 @@ const loop = setInterval(() =>{
         mario.style.marginLeft = '50px'
 
         document.getElementById('lost').textContent="Game Over"
+        document.getElementById('final').textContent=`Tempo Jogado: ${seconds}segundos e ${mili} Milisegundos`
     }
+    else
+    contador()
  },10)
 
-document.addEventListener('keydown', jump)
+function contador() {
+    mili++
+        var mil = document.getElementById('mili').textContent= mili*10
+        if (mil > 1000) {
+            seconds++
+            mili = 0
+            document.getElementById('seconds').textContent=seconds
+        }
+}
+
+document.addEventListener('keydown', jump,)
